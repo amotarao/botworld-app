3e1e2c7e1b23d849abd3443c074adb74d23d7ea2
Amon Sawamura <amon.sawamura@gmail.com>
2019-09-24 12:58:02 +0900

    feat(h GlobalStyle): add global style

--- hosting/src/App.tsx
+++ hosting/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { GlobalStyle } from './constants/GlobalStyle';
 import { BotSingle } from './components/pages/BotSingle';
 
 const App: React.FC = () => (
-  <Router>
-    <Route path="/bot/:id" component={BotSingle} />
-  </Router>
+  <>
+    <GlobalStyle />
+    <Router>
+      <Route path="/bot/:id" component={BotSingle} />
+    </Router>
+  </>
 );
 
 export default App;
